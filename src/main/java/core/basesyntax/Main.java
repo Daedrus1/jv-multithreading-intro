b3580c31e2d3a84072144f6ac8a761c1b7b59526
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2026-02-14 14:42:47 +0100

    Create and start in main() method 2 threads: the first extending Thread, and the second implementing Runnable.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable);
+        MyThread thread2 = new MyThread();
+        thread2.setDaemon(true);
+thread1.start();
+thread2.start();
     }
 }
